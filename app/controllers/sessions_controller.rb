240d4d8b6b44c003c8cbe7e0697fdefd011f1333
Salvatore Santamaria <sam.santamaria@gmail.com>
2019-11-13 08:53:55 -0800

    Added auth, need to update sessions/new.html.erb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,5 +2,23 @@ class SessionsController < ApplicationController
 
     def new
     end
+
+    def create
+        user = User.find_by(username: params[:session][:username])
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.user_id
+            flash[:success] = "You have logged in" #using semantic class success
+            redirect to root_path
+        else   
+            flash.now[:error] = "There was something wrong with your login information" #using semantic class error
+            render 'new'
+        end
+    end
+
+    def destroy 
+        session[:user_id] = nil
+        flash[:success] = "You have logged out"
+        redirect_to login_path
+    end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 get 'login', to: 'sessions#new' #upon receiving get login request, fire off controller 
+post 'login', to: 'sessions#create' #send to sessions controller, create action
+delete 'logout', to: 'sessions#destroy' #send to sessions controller, destroy action 
 root 'chatroom#index'
 
 

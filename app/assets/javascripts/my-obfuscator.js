b7b95a56c391e0da33490b34a9faceb8d3da86f7
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-01-03 15:20:56 -0800

    Added obfuscator

---
+++ app/assets/javascripts/my-obfuscator.js
@@ -0,0 +1,24 @@
+// Require Filesystem module
+var fs = require("fs");
+
+// Require the Obfuscator Module
+var JavaScriptObfuscator = require('javascript-obfuscator');
+
+// Read the file of your original JavaScript Code as text
+fs.readFile('/Users/salvatoresantamaria/Desktop/Rails Playground/message-me/app/assets/javascripts/application_og.js', "UTF-8", function (err, data) {
+    if (err) {
+        throw err;
+    }
+
+    // Obfuscate content of the JS file
+    var obfuscationResult = JavaScriptObfuscator.obfuscate(data);
+
+    // Write the obfuscated code into a new file
+    fs.writeFile('/Users/salvatoresantamaria/Desktop/Rails Playground/message-me/app/assets/javascripts/application.js', obfuscationResult.getObfuscatedCode(), function (err) {
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log("The file was saved!");
+    });
+});
